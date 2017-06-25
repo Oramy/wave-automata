e460756b5eec7a547144023c9c6c80341acb605e
Rémi <Rmi@DESKTOP-7HDF9T9>
2017-06-25 21:52:59 +0200

    premiere antenne

--- Fresnel/core/src/com/hx2/fresnel/Constants.java
+++ Fresnel/core/src/com/hx2/fresnel/Constants.java
@@ -1,7 +1,7 @@
 package com.hx2.fresnel;
 
 public class Constants {
-	public static final int WIDTH = 500;
-	public static final int HEIGHT = 500;
+	public static final int WIDTH = 1000;
+	public static final int HEIGHT = 1000;
 	public static final boolean FULLSCREEN = false;
 }
